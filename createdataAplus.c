581665a695572edef59453ca1f3cc5cc6bd41cee
Chinmay Bhandaru <praneethsbhandaru@gmail.com>
2023-11-29 21:30:26 -0500

    feat: attempting implementation of bl (put it in worng file)

--- createdataAplus.c
+++ createdataAplus.c
@@ -41,7 +41,7 @@ int main(void) {
 
     fwrite(&uiInstr, sizeof(unsigned int), 1, psFile);
 
-    uiInstr = MiniAssembler_bl(0xfffff7e7ff90, 0x420070);
+    uiInstr = MiniAssembler_bl(0x400670, 0x420070);
 
     fwrite(&uiInstr, sizeof(unsigned int), 1, psFile);
 
